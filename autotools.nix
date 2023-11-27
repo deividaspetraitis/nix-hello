49535b19afa864ff0dde1eb6354a3a86e7a7cc84
Deividas Petraitis <deividaspetraitis@Sandras-MBP.lan>
2023-11-27 20:22:10 +0200

    fix build

--- autotools.nix
+++ autotools.nix
@@ -2,29 +2,9 @@ pkgs: attrs:
   let defaultAttrs = {
     builder = "${pkgs.bash}/bin/bash";
     args = [ ./builder.sh ];
-    baseInputs = with pkgs; [ gnutar gzip gnumake clang coreutils gawk gnused gnugrep binutils.bintools ];
+    baseInputs = with pkgs; [ gnutar gzip gnumake clang coreutils gawk gnused gnugrep clang.bintools.bintools_bin ]; # will not work on other machines than Mac
     buildInputs = [];
     system = builtins.currentSystem;
   };
   in
   derivation (defaultAttrs // attrs )
-#pkgs: name:
-#  let
-#    res = with pkgs; [ "${pkgs.${name}}" ];
-#  in
-#   res
-#let
-#  negate = x: !x;
-#  sum = x: y: x + y;
-#  pkgs = <nixpkgs>;
-#in
-#  builtins.toString "qq"
-
-# pkgs:
-#pkgs: attrs:
-#  let defaultAttrs = {
-#
-#  };
-#  defaultAttrs
-  # in
-  # derivation (defaultAttrs // attrs )
