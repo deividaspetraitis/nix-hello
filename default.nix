098675f9f3c8a98e4ef1ea66c7b6154cad3fb8d9
Deividas Petraitis <hi@deividaspetraitis.lt>
2023-12-01 15:48:09 +0200

    apply override design pattern

--- default.nix
+++ default.nix
@@ -2,14 +2,17 @@ let
   nixpkgs = import <nixpkgs> {};
   allPkgs = nixpkgs // pkgs;
   callPackage = path: overrides:
-    let f = import path;
-    in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
+    let
+      f = import path;
+      lib = import ./lib.nix;
+    in
+      lib.makeOverridable f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
   pkgs = with nixpkgs; {
     mkDerivation = import ./autotools.nix nixpkgs;
     hello = callPackage ./hello.nix { };
     graphviz = callPackage ./graphviz.nix { };
-    graphvizCore = import ./graphviz.nix { 
-      gdSupport = false;
+    graphvizCore = graphviz.override {
+      gd = "gif";
     };
   };
 in pkgs
